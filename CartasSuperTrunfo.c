2be9cbda9bdf5c3a7f4a9f1bcec3ccf57d108acc
Alieksey Batistt de Barros <142556900+aliekseybatistt@users.noreply.github.com>
2025-03-15 02:21:11 +0000

    Reorganização dos atributos das cartas e finalização do printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,7 +10,7 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
-    char codigo_estado, codigo_cidade, pais, estado, cidade;
+    char codigo[50], estado, cidade[50];
     int populacao, ponto_turistico;
     float area, pib;
 
@@ -23,17 +23,22 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    scanf ("Digite o nome do estado: ", estado);
-    scanf ("Digite o código do estado: ", codigo_estado);
-    scanf ("Digite o nome da cidade: ", cidade);
-    scanf ("Digite o código da cidade: ", codigo_cidade);
-    scanf ("Digite o nome do país: ", pais);
-    scanf ("Digite o número de habitantes da cidade: ", populacao);
-    scanf ("Digite o PIB da cidade: ", pib);
-    scanf ("Digite a área da cidade: ", area);
-    scanf ("Digite o número de pontos turísticos da cidade: ", ponto_turistico);
+    printf("Digite o nome do estado: ");
+    scanf("%c", &estado);
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &cidade);
+    printf("Digite o código da cidade: ");
+    scanf("%s", &codigo);
+    printf("Digite o número de habitantes da cidade: ");
+    scanf("%d", &populacao);
+    printf("Digite o PIB da cidade: ");
+    scanf("%f", &pib);
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area);
+    printf("Digite o número de pontos turísticos da cidade: ");
+    scanf("%d", &ponto_turistico);
 
-    printf ("Código: \n País: \n Estado: \n Cidade: \n População: \n PIB: \n Área: \n Número de pontos turísticos: ");
+    printf("Estado: %c\nCódigo: %s\nCidade: %s\nPopulação: %d habitantes\nPIB: %f bilhões de reais\nÁrea: %f km²\nNúmero de pontos turísticos: %d" , estado, codigo, cidade, populacao, pib, area, ponto_turistico);
 
     return 0;
 }
